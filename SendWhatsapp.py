3f26ea23c15c2a743b6fb6193c5696e2d368f1ea
ashishkaul <jnjashishkau@gmail.com>
2019-12-10 17:42:40 +0100

    Skip unfound or wrong names without breaking application

--- SendWhatsapp.py
+++ SendWhatsapp.py
@@ -9,11 +9,23 @@ message = input('Enter message: ')
 
 input('Press enter after scanning QR code')
 
+def user_exists(recipient):
+    recipient_exists = False
+    try:
+        driver.find_element_by_xpath('//span[@title = "{}"]'.format(recipient.strip()))
+        recipient_exists = True
+    except Exception as ex:
+        print (ex)
+    finally:
+        return recipient_exists
 
 for recipient in recipients:
-    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(recipient.strip()))
-    user.click()
-    message_box = driver.find_element_by_xpath('//footer//div/div[2]/div')
-    message_box.send_keys(message)
-    send_button = driver.find_element_by_xpath('//span[@data-icon = "send"]')
-    send_button.click()
+    if(user_exists(recipient)):
+        user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(recipient.strip()))
+        user.click()
+        message_box = driver.find_element_by_xpath('//footer//div/div[2]/div')
+        message_box.send_keys(message)
+        send_button = driver.find_element_by_xpath('//span[@data-icon = "send"]')
+        send_button.click()
+    else:
+        continue
